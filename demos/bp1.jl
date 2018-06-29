54e4e9f1f3deae9c32b07bdc66aaac1032667d0a
Pengcheng Shi <spc@170.210.20.172.s.wireless.uri.edu>
2018-06-29 15:40:40 -0400

    works, but slow

--- demos/bp1.jl
+++ demos/bp1.jl
@@ -7,28 +7,30 @@ addprocs(4)
 dc3d_file = joinpath(dirname(@__DIR__), "src/dc3d.jl")
 # need string insertion here to work around
 @everywhere include($dc3d_file)
+using DifferentialEquations
+using Plots
 
 ## parameters are set using BP1 requirements
 const ρ = 2670.0
 const g = 9.8
-const cs = 3464.0
-const σ = 50.0e6
+const cs = 11.08e7
+const σ = 500.0
 const a0 = 0.010
 const amax = 0.025
 const b0 = 0.015
-const Dc = 0.008
-const Vp = 1e-9
-const Vinit = 1e-9
-const V0 = 1e-6
+const Dc = 8.0
+const Vp = 31.53
+const Vinit = 31.53
+const V0 = 31.53e3
 const f0 = 0.6
-const H = 15.0e3
-const h = 3.0e3
-const Wf = 40.0e3
-const Δz = 25.0
+const H = 15.0
+const h = 3.0
+const Wf = 40.0
+const Δz = 25.0e-3
 const tf = 3000.0
 
 # parameters implicit by above
-const μ = cs^2 * ρ
+const μ = 0.32
 const λ = μ
 const α = (λ + μ) / (λ + 2μ)
 const η = μ / 2cs
@@ -45,6 +47,7 @@ const sdip = sinpi(dip / 180)
 ## stiff matrix, parallelly assembled
 K = SharedArray{Float64}(ngrid, ngrid)
 
+# row: obs depths; col: fault patches
 function fill_stiff_matrix!(K)
     for i = 1: ngrid
         aw = [-Δz * i, -Δz * (i - 1)]
@@ -60,5 +63,53 @@ function fill_stiff_matrix!(K)
     end
 end
 
-fill_stiff_matrix!(K)
-## modeling
+@time fill_stiff_matrix!(K)
+## build depth profile
+z = (collect(1: ngrid) - 0.5) * Δz
+az = fill!(zeros(z), a0)
+az[z .≥ (H + h)] = amax
+az[H .< z .< H + h] = a0 + (amax - a0) / (h / Δz)* collect(1: Int(h / Δz))
+
+# initial condition
+δz = zeros(z)
+τ0 = σ * amax * asinh(Vinit / 2V0 * exp((f0 + b0 * log(V0 / Vinit)) / amax)) + η * Vinit
+τz = fill!(zeros(z), τ0)
+θz = @. Dc / V0 * exp(az / b0 * log(2V0 / Vinit * sinh((τz - η * Vinit) / az / σ)) - f0 / b0)
+vz = fill!(zeros(z), Vinit)
+
+## derivation
+const K2 = convert(Array, K)
+
+@views function rsf_odes!(du, u, p, t)
+    kk, vp, aa, bb, dc, eta = p
+    dμ_dt = kk * (vp .- u[:, 1])
+    dθ_dt = @. 1 - u[:, 1] * u[:, 2] / dc
+    dμ_dθ = @. bb / u[:, 2]
+    dμ_dv = @. aa / u[:, 1]
+    dv_dt = @. (dμ_dt - dμ_dθ * dθ_dt) / (dμ_dv + eta)
+    du[:, 1] = dv_dt
+    du[:, 2] = dθ_dt
+end
+
+function rsf_odes_regularized!(du, u, p, t)
+    ϕ1 = @. log(V0 * u[:, 2] / Dc)
+    ϕ2 = @. u[:, 1] / 2V0
+    ϕ3 = @. (f0 + b0 * ϕ1) / az
+    ϕ4 = @. sqrt(1 + (ϕ2 * exp(ϕ3))^2)
+    dμ_dv = @. az / ϕ4 * exp(ϕ3) / 2V0
+    dμ_dθ = @. b0 / u[:, 2] * ϕ2 * exp(ϕ3) / ϕ4
+    dθ_dt = @. 1 - u[:, 1] * u[:, 2] / Dc
+    dμ_dt = K * (Vp - u[:, 1])
+    dv_dt = @. (dμ_dt - dμ_dθ * dθ_dt) / (dμ_dv + η)
+    du[:, 1] = dv_dt
+    du[:, 2] = dθ_dt
+end
+
+u0 = hcat(vz, θz)
+tspan = (0., 1.0)
+prob = ODEProblem(rsf_odes!, u0, tspan, (K2, Vp, az, b0, Dc, η))
+@time sol = solve(prob, Tsit5(), reltol=1e-3, abstol=1e-3)
+
+##
+plot(sol.t, sol[1, 1, :])
+plot(az, z)
