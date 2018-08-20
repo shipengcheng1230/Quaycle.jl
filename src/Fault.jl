28cfcbaa09de20c11f1576f468f4e3ed61b19615
Pengcheng Shi <spc@254.155.20.172.s.wireless.uri.edu>
2018-08-20 17:12:42 -0400

    add getindex feature

--- src/Fault.jl
+++ src/Fault.jl
@@ -1,5 +1,7 @@
 module Fault
 
+using Base: getindex
+
 export AbstractFault, PlaneFault, CurvedFault
 export NormalFault, ThrustFault, StrikeSlipFault
 export AbstractFaultDomain, PlaneFaultDomain
@@ -24,12 +26,17 @@ struct PlaneFaultDomain{ftype, dim, T} <: AbstractFaultDomain{ftype, dim}
         zero(T) ≤ dip ≤ 90 * one(T) || error("Fautl dip angle ∈ [0, π/2].")
         any(@. span ≤ zero(T)) && error("Fault domain must be larger than zero.")
         (N == 1 || N == 2) || error("Fault domain should be given by along-downdip [and along-strike] length[s].")
-        new{FT,N,T}(dip, span)
+        new{FT, N, T}(dip, span)
     end
 end
 
 fault(ftype::Type{<:PlaneFault}, dip, span) = PlaneFaultDomain(ftype, dip, span)
 fault(ftype::Type{<:PlaneFault}, dip, span::Number) = PlaneFaultDomain(ftype, dip, tuple(span))
-fault(ftype::Type{<:PlaneFault}, dip, span::AbstractArray{<:Number, 1}) = PlaneFaultDomain(ftype, dip, tuple(span...))
+fault(ftype::Type{<:PlaneFault}, dip, span::AbstractVector{<:Number}) = PlaneFaultDomain(ftype, dip, tuple(span...))
+
+getindex(fd::PlaneFaultDomain{ftype, 1}, name::Symbol) where {ftype} = name == :ξ ? fd.span[1] : error(
+    "One dim fault domain only contains index of `ξ`.")
+getindex(fd::PlaneFaultDomain{ftype, 2}, name::Symbol) where {ftype} = name == :ξ ? fd.span[2] : name == :x ? fd.span[1] : error(
+    "Two dim fault domain only ontains index of `x` and `ξ`.")
 
 end # module
