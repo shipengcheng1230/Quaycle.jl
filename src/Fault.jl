7dc7949e41261d1e9ed8f4a00eb385b55f613017
Pengcheng Shi <spc@254.155.20.172.s.wireless.uri.edu>
2018-08-20 13:14:36 -0400

    fault utils

--- src/Fault.jl
+++ src/Fault.jl
@@ -1,82 +1,35 @@
 module Fault
 
-export AbstractFaultType, PlaneFaultType, CurvedFaultType
-export NormalFaultType, ThrustFaultType, StrikeSlipFaultType
+export AbstractFault, PlaneFault, CurvedFault
 export NormalFault, ThrustFault, StrikeSlipFault
 export AbstractFaultDomain, PlaneFaultDomain
-export AbstractGridType, FiniteDifferenceGrid, UniformFiniteDifferenceGrid
-export fault, discretize
+export fault
 
-abstract type AbstractFaultType end
-abstract type PlaneFaultType <: AbstractFaultType end
-abstract type CurvedFaultType <: AbstractFaultType end
+abstract type AbstractFault end
+abstract type PlaneFault <: AbstractFault end
+abstract type CurvedFault <: AbstractFault end
 
-abstract type NormalFaultType <: PlaneFaultType end
-abstract type ThrustFaultType <: PlaneFaultType end
-abstract type StrikeSlipFaultType <: PlaneFaultType end
+abstract type NormalFault <: PlaneFault end
+abstract type ThrustFault <: PlaneFault end
+abstract type StrikeSlipFault <: PlaneFault end
 
-struct NormalFault <: NormalFaultType end
-struct ThrustFault <: ThrustFaultType end
-struct StrikeSlipFault <: StrikeSlipFaultType end
+abstract type AbstractFaultDomain{ftype, dim} end
 
-abstract type AbstractFaultDomain end
-
-struct PlaneFaultDomain{FT,T,N} <: AbstractFaultDomain where {FT<:PlaneFaultType,T<:Number,N}
-    fault_type::FT
-    domain::NTuple{N,T} # https://discourse.julialang.org/t/why-is-it-not-matching-ntuple-n-t-when-specifying-n/13382
+struct PlaneFaultDomain{ftype, dim, T} <: AbstractFaultDomain{ftype, dim}
     dip::T
+    span::NTuple{dim, T}
 
-    function PlaneFaultDomain(ft::FT, dom::NTuple{N,T}, dip::T) where {FT,T<:Number,N}
-        zero(T) ≤ dip ≤ one(T) * 90 || error("Fautl dip angle ∈ [0, π/2].")
-        any(@. dom < zero(T)) && error("Fault domain must be larger than zero.")
+    function PlaneFaultDomain(ftype::Type{FT}, dip::T, span::U) where
+        {FT<:PlaneFault, T<:Number, U<:NTuple{N, T}} where {N}
+        zero(T) ≤ dip ≤ 90 * one(T) || error("Fautl dip angle ∈ [0, π/2].")
+        any(@. span ≤ zero(T)) && error("Fault domain must be larger than zero.")
         (N == 1 || N == 2) || error("Fault domain should be given by along-downdip [and along-strike] length[s].")
-        new{FT,T,N}(ft, dom, dip)
+        new{FT,N,T}(dip, span)
     end
 end
 
-function fault(ft::FT, dom::NTuple{N,T}, dip::T) where {FT<:PlaneFaultType,N,T<:Number}
-    PlaneFaultDomain(ft, dom, dip)
-end
+fault(ftype::Type{<:PlaneFault}, dip, span) = PlaneFaultDomain(ftype, dip, span)
+fault(ftype::Type{<:PlaneFault}, dip, span::Number) = PlaneFaultDomain(ftype, dip, tuple(span))
+fault(ftype::Type{<:PlaneFault}, dip, span::AbstractArray{<:Number, 1}) = PlaneFaultDomain(ftype, dip, tuple(span...))
 
-function fault(ft::FT, dom::T, dip::T) where {FT<:PlaneFaultType,T<:Number}
-    PlaneFaultDomain(ft, tuple(dom), dip)
-end
-
-abstract type AbstractGridType{ndim} end
-abstract type FiniteDifferenceGrid{ndim} <: AbstractGridType{ndim} end
-
-struct OnFaultUniformFDGrid{E,T,Val(2)} <: FiniteDifferenceGrid{Val(2)} where {E<:Number,T<:AbstractArray{E}}
-    x::T # along-strike
-    ξ::T # along-downdip
-    Δx::E
-    Δξ::E
-end
-
-
-function discretize(fd::PlaneFaultDomain, nelm::NTuple{N,T}) where {N,T<:Integer}
-    dims = length(fd.domain)
-    dims == N || error("Grid numbers mismatch with fault domain.")
-    any(@. nelm ≤ zero(T)) && error("Grid numbers must be larger than 0.")
-
-    lξ, nξ = fd.domain[1], nelm[1]
-    Δξ = lξ / nξ
-    ξ = collect(range(zero(lξ), stop=-lξ+Δξ, length=nξ)) .- Δξ/2
-    if N == 1
-        OnFaultUniformFDGrid(missing, ξ, missing, Δξ)
-    elseif N == 2
-        lx, nx = fd.domain[2], nelm[2]
-        Δx = lx / nx
-        x = collect(range(-lx/2+Δx/2, stop=lx/2-Δx/2, length=nx))
-        OnFaultUniformFDGrid(x, ξ, Δx, Δξ)
-    end
-end
-
-function discretize(dom::NTuple{1}, nelm::NTuple{1,<:Integer})
-
-end
-
-function discretize(dom::NTuple{2}, nelm::NTuple{2,<:Integer})
-
-end
-
-end
+end # module
--- test/test_fault.jl
+++ test/test_fault.jl
@@ -1,21 +1,15 @@
 using Test
 
 @testset "Fault creation" begin
-    @test_throws ErrorException("Fautl dip angle ∈ [0, π/2].") fault(NormalFault(), (1, 2), -1)
-    @test_throws ErrorException("Fault domain must be larger than zero.") fault(ThrustFault(), (-1, 2), 2)
-    @test_throws ErrorException("Fault domain should be given by along-downdip [and along-strike] length[s].") fault(StrikeSlipFault(), (1, 2, 3), 2)
+    @test_throws ErrorException("Fautl dip angle ∈ [0, π/2].") fault(NormalFault, -1, (1, 2))
+    @test_throws ErrorException("Fault domain must be larger than zero.") fault(ThrustFault, 2, (-1, 2))
+    @test_throws ErrorException("Fault domain should be given by along-downdip [and along-strike] length[s].") fault(StrikeSlipFault, 2, (1, 2, 3))
 end
 
-@testset "Fault uniform FD grids" begin
-    global fa = fault(ThrustFault(), (30., 60.), 10.)
-    @testset "Generate error" begin
-        @test_throws ErrorException("Grid numbers mismatch with fault domain.") discretize(fa, (64, 128, 20))
-        @test_throws ErrorException("Grid numbers must be larger than 0.") discretize(fa, (-1, 128))
-    end
-
-    @testset "Interval corretion" begin
-        global g = discretize(fa, (64, 128))
-        @test abs(g.x[2] - g.x[1]) ≈ 60 / 128
-        @test abs(g.ξ[2] - g.ξ[1]) ≈ 30 / 64
-    end
+@testset "Equivalent initialization" begin
+    fd1 = fault(NormalFault, 1, (1,))
+    fd2 = fault(NormalFault, 1, 1)
+    fd3 = fault(NormalFault, 1, [1])
+    @test fd1 == fd2
+    @test fd2 == fd3
 end
