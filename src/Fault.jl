65189723276912e9cf76a51b5da544dc33ea17e9
Pengcheng Shi <spc@254.155.20.172.s.wireless.uri.edu>
2018-08-28 14:38:47 -0400

    constrain TF to be vertical

--- src/Fault.jl
+++ src/Fault.jl
@@ -26,13 +26,18 @@ struct PlaneFaultDomain{ftype, dim, T} <: AbstractFaultDomain{ftype, dim}
         zero(T) ≤ dip ≤ 90 * one(T) || error("Fautl dip angle ∈ [0, π/2].")
         any(@. span ≤ zero(T)) && error("Fault domain must be larger than zero.")
         (N == 1 || N == 2) || error("Fault domain should be given by along-downdip [and along-strike] length[s].")
+        (FT == StrikeSlipFault && dip ≉ 90 * one(T)) && error("Strike-Slip faults should be vertical.")
         new{FT, N, T}(dip, span)
     end
 end
 
-fault(ftype::Type{<:PlaneFault}, dip, span) = PlaneFaultDomain(ftype, dip, span)
-fault(ftype::Type{<:PlaneFault}, dip, span::Number) = PlaneFaultDomain(ftype, dip, tuple(span))
-fault(ftype::Type{<:PlaneFault}, dip, span::AbstractVector{<:Number}) = PlaneFaultDomain(ftype, dip, tuple(span...))
+fault(ftype::Type{<:PlaneFault}, dip, span::NTuple) = PlaneFaultDomain(ftype, dip, span)
+fault(ftype::Type{<:PlaneFault}, dip, span::Number) = fault(ftype, dip, tuple(span))
+fault(ftype::Type{<:PlaneFault}, dip, span::AbstractVector{<:Number}) = fault(ftype, dip, tuple(span...))
+
+fault(ftype::Type{StrikeSlipFault}, span) = fault(ftype, one(eltype(span)) * 90, span)
+fault(ftype::Type{StrikeSlipFault}, span::Number) = fault(ftype, tuple(span))
+fault(ftype::Type{StrikeSlipFault}, span::AbstractVector{<:Number}) = fault(ftype, tuple(span...))
 
 getindex(fd::PlaneFaultDomain{ftype, 1}, name::Symbol) where {ftype} = name == :ξ ? fd.span[1] : error(
     "One dim fault domain only contains index of `ξ`.")
--- test/test_fault.jl
+++ test/test_fault.jl
@@ -4,14 +4,23 @@ using Test
     @test_throws ErrorException("Fautl dip angle ∈ [0, π/2].") fault(NormalFault, -1, (1, 2))
     @test_throws ErrorException("Fault domain must be larger than zero.") fault(ThrustFault, 2, (-1, 2))
     @test_throws ErrorException("Fault domain should be given by along-downdip [and along-strike] length[s].") fault(StrikeSlipFault, 2, (1, 2, 3))
+    @test_throws ErrorException("Strike-Slip faults should be vertical.") fault(StrikeSlipFault, 10, 1)
 end
 
 @testset "Equivalent initialization" begin
-    fd1 = fault(NormalFault, 1, (1,))
-    fd2 = fault(NormalFault, 1, 1)
-    fd3 = fault(NormalFault, 1, [1])
-    @test fd1 == fd2
-    @test fd2 == fd3
+    @testset "Default construction" begin
+        fd1 = fault(NormalFault, 1, (1,))
+        fd2 = fault(NormalFault, 1, 1)
+        fd3 = fault(NormalFault, 1, [1])
+        @test fd1 == fd2
+        @test fd2 == fd3
+    end
+
+    @testset "Vertical strike-slip fault" begin
+        fd1 = fault(StrikeSlipFault, 1.)
+        fd2 = fault(StrikeSlipFault, 90., 1.)
+        @test fd1 == fd2
+    end
 end
 
 @testset "Get index" begin
