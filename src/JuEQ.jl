f2b4067178bd4779402138214e061779eabca156
Pengcheng Shi <spc@254.155.20.172.s.wireless.uri.edu>
2018-08-29 15:48:55 -0400

    update

--- src/JuEQ.jl
+++ src/JuEQ.jl
@@ -3,11 +3,9 @@ module JuEQ
 using Reexport
 
 include("RateStateFriction.jl")
-include("Displacement.jl")
 include("BEM.jl")
 
 @reexport using .RateStateFriction
-@reexport using .Displacement
 @reexport using .BEM
 
 end
