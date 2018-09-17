936c638211227c3e37b60800dbd04620cb311d2f
Pengcheng Shi <spc@254.155.20.172.s.wireless.uri.edu>
2018-09-17 15:57:24 -0400

    error msg improvement

--- src/Fault.jl
+++ src/Fault.jl
@@ -21,10 +21,10 @@ struct PlaneFaultDomain{ftype, dim, T} <: AbstractFaultDomain{ftype, dim}
 
     function PlaneFaultDomain(ftype::Type{FT}, dip::T, span::U) where
         {FT<:PlaneFault, T<:Number, U<:NTuple{N, T}} where {N}
-        zero(T) ≤ dip ≤ 90 * one(T) || error("Fautl dip angle ∈ [0, π/2].")
-        any(@. span ≤ zero(T)) && error("Fault domain must be larger than zero.")
-        (N == 1 || N == 2) || error("Fault domain should be given by along-downdip [and along-strike] length[s].")
-        (FT == StrikeSlipFault && dip ≉ 90 * one(T)) && error("Strike-Slip faults should be vertical.")
+        zero(T) ≤ dip ≤ 90 * one(T) || error("Fautl dip angle: $dip ∉ [0, π/2].")
+        any(@. span ≤ zero(T)) && error("Fault domain: $span must be larger than zero.")
+        (N == 1 || N == 2) || error("Fault domain dim: $N should be `1` (along-downdip) or `2` (plus along-strike).")
+        (FT == StrikeSlipFault && dip ≉ 90 * one(T)) && error("Dip angle: $dip for strike-slip faults should be `90`.")
         new{FT, N, T}(dip, span)
     end
 end
@@ -38,6 +38,6 @@ fault(ftype::Type{StrikeSlipFault}, span::Number) = fault(ftype, tuple(span))
 fault(ftype::Type{StrikeSlipFault}, span::AbstractVector{<:Number}) = fault(ftype, tuple(span...))
 
 getindex(fd::PlaneFaultDomain{ftype, 1}, name::Symbol) where {ftype} = name == :ξ ? fd.span[1] : error(
-    "One dim fault domain only contains index of `ξ`.")
+    "One dim fault domain only contains index of `ξ`, received $name.")
 getindex(fd::PlaneFaultDomain{ftype, 2}, name::Symbol) where {ftype} = name == :ξ ? fd.span[2] : name == :x ? fd.span[1] : error(
-    "Two dim fault domain only contains index of `x` and `ξ`.")
+    "Two dim fault domain only contains index of `x` and `ξ`., received $name.")
