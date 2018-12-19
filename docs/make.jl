e184aa24546c3752d548a1d66e557afb893365f9
shipengcheng1230 <shipengcheng1230@gmail.com>
2018-12-19 00:54:53 -0500

    using mkdocs for better doc style

--- docs/make.jl
+++ docs/make.jl
@@ -3,44 +3,25 @@ if Base.HOME_PROJECT[] !== nothing
     Base.HOME_PROJECT[] = abspath(Base.HOME_PROJECT[])
 end
 
-using Documenter, JuEQ
-using Documenter
-using Literate
+using Documenter, DocumenterMarkdown, JuEQ
 using Plots # to not capture precompilation output
 
 ENV["GKSwstype"] = "100"
 
-function convert_examples_to_markdown(name::AbstractString; outdir=joinpath(@__DIR__, "src/generated"))
-    ex = joinpath(@__DIR__, "..", "examples", name)
-    Literate.markdown(ex, outdir)
-end
+include("generate.jl")
 
-examples = ["bp1.jl", "otfsync.jl"]
-map(convert_examples_to_markdown, examples)
+cp(joinpath(@__DIR__, "..", "LICENSE"), joinpath(@__DIR__, "src", "LICENSE.md"); force = true)
 
 makedocs(
+    doctest=false,
     modules = [JuEQ],
-    format = :html,
     sitename = "JuEQ",
-    pages = [
-        "Home" => "index.md",
-        "Manual" => [
-            "Quasi-dynamic" => [
-                "Introduction" => "quasi_dynamic_intro.md",
-                "Example 1: 1D fault" => "generated/bp1.md",
-                "Example 2: 2D fault" => "generated/otfsync.md",
-            ]
-        ],
-        "Libray" => [
-            "Public" => "public_interface.md",
-            "Private" => "private_interface.md",
-        ],
-    ],
+    format = Markdown(),
 )
 
 deploydocs(
   repo  = "github.com/shipengcheng1230/JuEQ.jl.git",
-  target = "build",
-  deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
-  make = nothing,
+  target = "site",
+  deps = Deps.pip("mkdocs", "pygments", "python-markdown-math", "mkdocs-cinder"),
+  make = () -> run(`mkdocs build`),
 )
