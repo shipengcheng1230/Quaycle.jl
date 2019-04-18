2e4c3daa2f1b8d5104747c0a5cde60bcd5c11d30
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-04-18 14:53:06 -0400

    remove auxillary macro

--- src/utils/savecallback.jl
+++ src/utils/savecallback.jl
@@ -37,67 +37,51 @@ function mmapsave(iot::IOStream, iou::IOStream, thrd::Number; dts=0.1, dta=3.153
     FunctionCallingCallback(__save; func_everystep=true)
 end
 
-macro setlastindex(dest, src, nd, dest_index)
-    Expr(
-        :(=),
-        Expr(:ref, esc(dest), [:(:) for _ in 1: nd]..., esc(dest_index)),
-        esc(src),
-    )
-end
-
-macro setlastindex(dest, src, nd, dest_index, src_index)
-    Expr(
-        :(=),
-        Expr(:ref, esc(dest), [:(:) for _ in 1: nd]..., esc(dest_index)),
-        Expr(:call, :selectdim, esc(src), nd + 1, esc(src_index)),
-    )
-end
-
 macro h5save(filename, tend, nsteps, usize, nd, T)
     callback = gensym(:callback)
     esc(quote
         let count = 1
             global $(callback)
             accu = Array{$T}(undef, $(usize)..., $(nsteps))
-            accusize = tuple($((usize))..., $(nsteps))
+            accusize = tuple($(usize)..., $(nsteps))
             acct = Vector{$T}(undef, $(nsteps))
             acctsize = ($(nsteps),)
             total = 0
             h5open($(filename), "w") do fid
-                d_create(fid, "u", $(T), (accusize, ntuple(_ -> -1, Val($nd+1))), "chunk", accusize)
-                d_create(fid, "t", $(T), (acctsize, (-1,)), "chunk", acctsize)
+                d = d_create(fid, "u", $(T), (accusize, ntuple(_ -> -1, Val($(nd+1)))), "chunk", accusize)
+                d = d_create(fid, "t", $(T), (acctsize, (-1,)), "chunk", acctsize)
             end
 
             function $callback(u, t, integrator)
                 if t == $(tend)
                     rest = total % $(nsteps)
-                    selectdim(accu, $nd+1, count) .= u
+                    selectdim(accu, $(nd+1), count) .= u
                     selectdim(acct, 1, count) .= t
                     h5open($filename, "r+") do f
                         d = d_open(f, "u")
-                        @setlastindex(d, accu, $nd, total-rest+1: total+1, 1: rest+1)
+                        d[$((:(:) for _ in 1: nd)...), total-rest+1: total+1] = selectdim(accu, $(nd+1), 1: rest+1)
                         set_dims!(d, ($(usize)..., total+1))
 
                         ht = d_open(f, "t")
-                        @setlastindex(ht, acct, 0, total-rest+1: total+1, 1: rest+1)
+                        ht[total-rest+1: total+1] = selectdim(acct, 1, 1: rest+1)
                         set_dims!(ht, (total+1,))
                     end
-                elseif count > $((nsteps))
+                elseif count > $(nsteps)
                     h5open($filename, "r+") do f
                         d = d_open(f, "u")
-                        @setlastindex(d, accu, $nd, total-$(nsteps)+1: total)
+                        d[$((:(:) for _ in 1: nd)...), total-$(nsteps-1): total] = accu
                         set_dims!(d, ($(usize)..., total+$(nsteps)))
 
                         ht = d_open(f, "t")
-                        @setlastindex(ht, acct, 0, total-$(nsteps)+1: total)
+                        ht[total-$(nsteps-1): total] = acct
                         set_dims!(ht, (total+$(nsteps),))
                     end
-                    selectdim(accu, $nd+1, 1) .= u
+                    selectdim(accu, $(nd+1), 1) .= u
                     selectdim(acct, 1, 1) .= t
                     count = 2
                     total += 1
                 else
-                    selectdim(accu, $nd+1, count) .= u
+                    selectdim(accu, $(nd+1), count) .= u
                     selectdim(acct, 1, count) .= t
                     count += 1
                     total += 1
