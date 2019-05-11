9527754c53db9351fb3f9d29d7d827539b0a5aa4
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-05-10 21:55:11 -0400

    fix copyright place

--- src/gfkernels/sbarbot_tet4.jl
+++ src/gfkernels/sbarbot_tet4.jl
@@ -1,34 +1,4 @@
-#-----------------------------------------------------------------------
-#  Author: James D. P. Moore (earth@jamesdpmoore.com) - 10 Jun, 2016.
-#  Modified: Sylvain Barbot (sbarbot@ntu.edu.sg) -
-#  Earth Observatory of Singapore
-#  Copyright (c) 2017 James D. P. Moore and Sylvain Barbot
-#
-#  This code and related code should be cited as:
-#    Barbot S., J. D. P. Moore and V. Lambert, Displacement and Stress
-#    Associated with Distributed Anelastic Deformation in a Half Space,
-#    Bull. Seism. Soc. Am., 107(2), 10.1785/0120160237, 2017.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to permit
-# persons to whom the Software is furnished to do so, subject to the
-# following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-# NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-# USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-#-----------------------------------------------------------------------
+# Author: Sylvain Barbot (sbarbot@ntu.edu.sg) - May 19, 2018, Los Angeles.
 # Translated by Pengcheng Shi (shipengcheng1230@gmail.com)
 
 # greens function ϵ vs u for tet4 element
