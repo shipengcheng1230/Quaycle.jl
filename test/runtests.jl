777929140ac971cf0e611c5fbaf5fb93b5e0ebf8
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-05-14 12:26:48 -0400

    add non-registered pkg test; may change in future

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,10 @@ using JuEQ
 
 const TESTDIR = @__DIR__
 
-for f in filter!(x -> startswith(x, "test_") && endswith(x, ".jl"), readdir(TESTDIR))
-    include(abspath(joinpath(TESTDIR, f)))
+if isempty(ARGS)
+    TESTFILES = filter(x -> startswith(x, "test_") && endswith(x, ".jl"), readdir(TESTDIR))
+else
+    TESTFILES = filter(x -> startswith(x, "_") && endswith(x, ".jl"), readdir(TESTDIR))
 end
+
+foreach(x -> include(joinpath(TESTDIR, x)), TESTFILES)
