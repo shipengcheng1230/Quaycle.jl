b78a62a805bbcdf33c2dc53a9b1e10e6e65939e6
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-05-15 15:12:40 -0400

    adaptive mesh for box (hex8) by extrusion

--- src/tools/gmshtools.jl
+++ src/tools/gmshtools.jl
@@ -1,7 +1,7 @@
 export gen_gmsh_mesh
 
 "Generate equivalent [`LineTopCenterMesh`](@ref) via [Gmsh](http://gmsh.info/)."
-function gen_gmsh_mesh(::Val{:topcenter}, ξ::T, Δξ::T, dip::T; filename::AbstractString="temp.msh") where T
+function gen_gmsh_mesh(::Val{:LineOkada}, ξ::T, Δξ::T, dip::T; filename::AbstractString="temp.msh") where T
     @gmsh_do begin
         model = gmsh.model
         factory = model.geo
@@ -25,7 +25,7 @@ function gen_gmsh_mesh(::Val{:topcenter}, ξ::T, Δξ::T, dip::T; filename::Abst
 end
 
 "Generate equivalent [`RectTopCenterMesh`](@ref) via [Gmsh](http://gmsh.info/)."
-function gen_gmsh_mesh(::Val{:topcenter}, x::T, ξ::T, Δx::T, Δξ::T, dip::T; filename::AbstractString="temp.msh") where T
+function gen_gmsh_mesh(::Val{:RectOkada}, x::T, ξ::T, Δx::T, Δξ::T, dip::T; filename::AbstractString="temp.msh") where T
     @gmsh_do begin
         model = gmsh.model
         factory = model.geo
@@ -61,4 +61,45 @@ function gen_gmsh_mesh(::Val{:topcenter}, x::T, ξ::T, Δx::T, Δξ::T, dip::T; 
         gmsh.model.mesh.generate(2)
         gmsh.write(filename)
     end
+end
+
+"""
+Gernate a box for [Asthenosphere](https://en.wikipedia.org/wiki/Asthenosphere) using 8-node hexahedron
+    elements (via setting transfinite curve).
+"""
+function gen_gmsh_mesh(::Val{:BoxHexByExtrude},
+    llx::T, lly::T, llz::T, dx::T, dy::T, dz::T, nx::I, ny::I,
+    rfx::T, rfy::T, rfzn::AbstractVector, rfzh::AbstractVector;
+    filename::AbstractString="temp.msh") where {T, I}
+
+    @gmsh_do begin
+        factory = gmsh.model.geo
+        @addPoint begin
+            llx, lly, llz, 1
+            llx + dx, lly, llz, 2
+            llx + dx, lly + dy, llz, 3
+            llx, lly + dy, llz, 4
+        end
+        @addLine begin
+            1, 2, 1
+            2, 3, 2
+            4, 3, 3
+            1, 4, 4
+        end
+        factory.addCurveLoop([1, 2, -3, -4], 1)
+        factory.addPlaneSurface([1], 1)
+        @setTransfiniteCurve begin
+            1, nx + 1, "Bump", rfx
+            2, ny + 1, "Bump", rfy
+            3, nx + 1, "Bump", rfx
+            4, ny + 1, "Bump", rfy
+        end
+        factory.mesh.setTransfiniteSurface(1, "Left", [1, 2, 3, 4])
+        factory.extrude(2, 0.0, 0.0, -dz, rfzn, rfzh, true)
+        factory.mesh.setRecombine(2, 1)
+        factory.synchronize()
+        gmsh.model.mesh.generate(3)
+        gmsh.write(filename)
+    end
+    return nothing
 end
--- test/_gmshtools.jl
+++ test/_gmshtools.jl
@@ -1,11 +1,12 @@
 using Test
 using GmshTools
+using LinearAlgebra
 
-# This file currently isn't included in `runtests.jl`
+## This file currently isn't included in `runtests.jl`
 
-@testset "Gmsh Top Center Line" begin
+@testset "Gmsh Okada Line" begin
     filename = tempname() * ".msh"
-    gen_gmsh_mesh(Val(:topcenter), 100.0, 20.0, 45.0; filename=filename)
+    gen_gmsh_mesh(Val(:LineOkada), 100.0, 20.0, 45.0; filename=filename)
     els1d = @gmsh_open filename begin
         gmsh.model.mesh.getElements(1)
     end
@@ -14,13 +15,28 @@ using GmshTools
     rm(filename)
 end
 
-@testset "Gmsh Top Center Line" begin
+@testset "Gmsh Okada Rect" begin
     filename = tempname() * ".msh"
-    gen_gmsh_mesh(Val(:topcenter), 100.0, 50.0, 20.0, 10.0, 45.0; filename=filename)
+    gen_gmsh_mesh(Val(:RectOkada), 100.0, 50.0, 20.0, 10.0, 45.0; filename=filename)
     els2d = @gmsh_open filename begin
         gmsh.model.mesh.getElements(2)
     end
     @test els2d[1][1] == 3 # 4-node quadrangle
     @test length(els2d[2][1]) == round(Int, 100.0 / 20.0 * 50.0 / 10.0)
+    rm(filename)
+end
+
+@testset "Gmsh Box by Extrude" begin
+    rfzn = ones(Float64, 10)
+    rfzh = accumulate((x, y) -> x * y, fill(2.0, size(rfzn))) |> cumsum
+    normalize!(rfzh, Inf)
+
+    filename = tempname() * ".msh"
+    gen_gmsh_mesh(Val(:BoxHexByExtrude), -100.0, -100.0, 0.0, 200.0, 200.0, 100.0, 10, 10, 1.1, 5.0, rfzn, rfzh; filename=filename)
+    el3d = @gmsh_open filename begin
+        gmsh.model.mesh.getElements(3)
+    end
+    @test el3d[1][1] == 5 # 8-node hexahedron
+    @test length(el3d[2][1]) == 10 * 10 * 10
     rm(filename)
 end
