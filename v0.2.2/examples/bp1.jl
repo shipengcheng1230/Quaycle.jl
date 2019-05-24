bcfce1574af72d5a01c0e02c9b8a8e34a933609e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-05-24 16:17:35 +0000

    build based on 3d8f048

---
+++ v0.2.2/examples/bp1.jl
@@ -0,0 +1,87 @@
+# !!! note
+#     This example is from [Benchmark Problem 1](https://www.scec.org/publication/8214) (hence referred as BP1).
+#
+
+# First, we load the package
+using JuEQ
+using Plots
+
+# The prerequisite parameters in this benchmark are list below:
+
+ms2mmyr = 365 * 86400 * 1e3 # convert velocity from m/s to mm/yr
+ρ = 2670.0 # density [kg/m³]
+vs = 3464.0 # shear wave velocity [m/s]
+σ0 = 500.0 # effective normal stress [bar]
+a0 = 0.010 # frictional paramter `a` in vw zone
+amax = 0.025 # frictional paramter `a` in vs zone
+b0 = 0.015 # frictional paramter `b`
+L0 = 8.0 # critical distance [mm]
+vpl = 1e-9 * ms2mmyr # plate rate [mm/yr]
+vinit = 1e-9 * ms2mmyr # initial velocity [mm/yr]
+v0 = 1e-6 * ms2mmyr # reference velocity [mm/yr]
+f0 = 0.6 # reference frictional coefficient
+H = 15.0 # vw zone [km]
+h = 3.0 # vw-vs changing zone [km]
+Wf = 40.0 # fault depth [km]
+Δz = 100.0e-3 # grid size interval [km]
+tf = 200.0; # simulation time [yr]
+nothing
+
+# !!! warning
+#     Make sure your units are consistent across the whole variable space.
+
+# Then we arrive at some parameters that are implicit by above:
+
+μ = vs^2 * ρ / 1e5 / 1e6 # shear modulus [bar·km/mm]
+λ = μ # poisson material
+η = μ / 2(vs * 1e-3 * 365 * 86400)
+ngrid = round(Int, Wf / Δz); # number of grids
+nothing
+
+# First, create a **fault space** by specifying fault type, depth
+# and with the desired discretization interval.
+# !!! tip
+#     Here, we do not need to provide `dip` for strike-slip fault as it automatically choose `90`. See [`fault`](@ref).
+
+fa = fault(Val(:LineOkada), STRIKING(), 40.0, Δz)
+nothing
+
+# Then, provide the material properties w.r.t. our 'fault space'.
+a = a0 .* ones(fa.mesh.nξ)
+a[-fa.mesh.z .≥ (H + h)] .= amax
+a[H .< -fa.mesh.z .< H + h] .= a0 .+ (amax - a0) / (h / Δz) * collect(1: Int(h / Δz))
+b = b0 .* ones(fa.mesh.nξ)
+L = L0 .* ones(fa.mesh.nξ)
+σ = σ0 .* ones(fa.mesh.nξ)
+prop = ElasticRSFProperty(a=a, b=b, L=L, σ=σ, λ=λ, μ=μ, vpl=vpl, f0=f0, v0=v0, η=η)
+nothing
+
+# Next, construct the initial condition and ODE problem using Okada's Green's function.
+τ0 = σ0 * amax * asinh(vinit / 2v0 * exp((f0 + b0 * log(v0 / vinit)) / amax)) + η * vinit
+τz = fill(τ0, size(fa.mesh.z))
+θz = @. L / v0 * exp(a / b0 * log(2v0 / vinit * sinh((τz - η * vinit) / a / σ)) - f0 / b0)
+vz = fill(vinit, size(fa.mesh.ξ))
+u0 = hcat(vz, θz);
+prob, = assemble(fa, prop,  u0, (0.0, tf))
+nothing
+
+# Check our depth profile now.
+
+plot([a, b], fa.mesh.z, label=["a", "b"], yflip=true, ylabel="Depth (km)")
+
+# Afterwards, solve ODE thanks to [DifferentialEquations.jl](https://github.com/JuliaDiffEq/DifferentialEquations.jl)
+
+sol = solve(prob, Tsit5(), reltol=1e-6, abstol=1e-6)
+nothing
+
+# !!! tip
+#     Raise the accuracy option or switch to other algorithms if you get instability when solving these ODEs.
+
+# Finally, check the results. The first event happens at around 196 year:
+
+maxv = JuEQ.max_velocity(sol)
+plot(sol.t, log10.(maxv / ms2mmyr), xlabel="Time (year)", ylabel="Max Velocity (log10 (m/s))", xlims=(190, 200), label="")
+
+# !!! note
+#     Click [here](https://plot.ly/~shipengcheng/3/depth-km-vs-slip-m/) for the slip evolution over 3000 years simulation.
+#     It may need some time to load the page.
---
+++ v0.2.2/examples/otfsync.jl
@@ -0,0 +1,101 @@
+# !!! note
+#     This example is adapted from [Wei, 2016 AGU](https://agu.confex.com/agu/fm16/meetingapp.cgi/Paper/132124)
+
+# !!! tip
+#     It will automatically use parallel scheme if `nprocs() ≂̸ 1` when building stiffness tensor. To do so:
+#     ```julia linenums="1"
+#     using Distributed
+#     addprocs(4); # add # of cores you desire
+#     using JuEQ
+#     ```
+
+# First, list all the essential parameters:
+
+using JuEQ
+using Plots
+
+ms2mmyr = 365 * 86400 * 1e3
+ρ = 2670.0 # kg/m³
+cs = 3044.0 # m/s
+vpl = 100.0 # mm/yr
+v0 = 3.2e4 # mm/yr
+f0 = 0.6;
+μ = 0.3 # Bar·km/mm
+λ = μ # poisson material
+α = (λ + μ) / (λ + 2μ)
+η = μ / 2(cs * 1e-3 * 365 * 86400) # Bar·yr/mm
+nothing
+
+# First, create a fault space.
+
+fa = fault(Val(:RectOkada), STRIKING(), 80., 10., 0.5, 0.5)
+nothing
+
+# Next, establish frictional and fault space parameters:
+a = ones(fa.mesh.nx, fa.mesh.nξ) .* 0.015
+b = ones(fa.mesh.nx, fa.mesh.nξ) .* 0.0115
+L = ones(fa.mesh.nx, fa.mesh.nξ) .* 12.0
+
+left_patch = @. -25. ≤ fa.mesh.x ≤ -5.
+right_patch = @. 5. ≤ fa.mesh.x ≤ 25.
+vert_patch = @. -6. ≤ fa.mesh.z ≤ -1
+
+b[xor.(left_patch, right_patch), vert_patch] .= 0.0185
+
+σmax = 500.
+σ = [min(σmax, 15. + 180. * z) for z in -fa.mesh.ξ]
+σ = Matrix(repeat(σ, 1, fa.mesh.nx)')
+prop = ElasticRSFProperty(a=a, b=b, L=L, σ=σ, λ=λ, μ=μ, vpl=vpl, f0=f0, v0=v0, η=η)
+nothing
+
+# Make sure our profile match our expectation:
+
+p1 = plot((a .- b)', seriestype=:heatmap,
+    xticks=(collect(1: 40: fa.mesh.nx+1), [-40, -20, 0, 20, 40]),
+    yticks=(collect(1: 5: fa.mesh.nξ+1), [0, 5, 10, 15, 20]),
+    yflip=true, color=:isolum, aspect_ratio=2, title="a-b",
+    );
+
+p2 = heatmap(σ',
+    xticks=(collect(1: 40: fa.mesh.nx+1), [-40, -20, 0, 20, 40]),
+    yticks=(collect(1: 5: fa.mesh.nξ+1), [0, 5, 10, 15, 20]),
+    yflip=true, color=:isolum, aspect_ratio=2, title="\\sigma"
+    );
+
+plot(p1, p2, layout=(2, 1))
+
+# Then, provide the initial condition and assemble the ODEs:
+
+vinit = vpl .* ones(fa.mesh.nx, fa.mesh.nξ)
+θ0 = L ./ vinit ./ 1.1
+u0 = cat(vinit, θ0, dims=3)
+prob, = assemble(fa, prop, u0, (0., 18.); buffer_ratio=1)
+nothing
+
+# !!! tip
+#     It is recommended (from Yajing Liu's personal communication) to add buffer zones adjacent the horizontal edges
+#     to immitate *zero* dislocation at the ridge region.
+#     Basically, it affects how the stiffness tensor are periodically summed. To what extent it alters the results remains further testing.
+
+#     Under the hood, it shall impose buffer areas on both sides of along-strike, each of which has a length of `bufferratio/2*fa[:x]`.
+#     Thus, the stiffness contributions falling into those buffer zone shall be neglected, which is equivalent to impose zero-slip correspondingly.
+
+# Afterwards, solve ODEs problem:
+
+sol = solve(prob, Tsit5(), reltol=1e-6, abstol=1e-6)
+nothing
+
+# Last, take a look at the max velocity time series:
+
+maxv = JuEQ.max_velocity(sol)
+plot(sol.t, log10.(maxv / ms2mmyr), xlabel="Time (year)", ylabel="Max Velocity (log10 (m/s))", label="")
+
+# And view some snapshots of ruptures (quasi-dynamic) patterns:
+
+ind = argmax(maxv)
+myplot = (ind) -> heatmap(log10.(sol.u[ind][:,:,1]./ms2mmyr)',
+    xticks=(collect(1: 40: fa.mesh.nx+1), [-40, -20, 0, 20, 40]),
+    yticks=(collect(1: 5: fa.mesh.nξ+1), [0, 5, 10, 15, 20]),
+    yflip=true, color=:isolum, aspect_ratio=2, title="t = $(sol.t[ind])")
+snaps = myplot(ind+300)
+plot(snaps)
