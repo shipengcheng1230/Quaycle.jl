83034cc5acfdda2e5540db8c6e19bc08cbdc9190
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-06-13 16:48:31 -0400

    fix assemble to new ArrayPartition

--- test/test_assemble.jl
+++ test/test_assemble.jl
@@ -4,7 +4,7 @@ using Test
     @testset "1D fault" begin
         fa = fault(Val(:LineOkada), DIPPING(), 10., 2.0, 45.0)
         p = ElasticRSFProperty([rand(fa.mesh.nξ) for _ in 1: 4]..., rand(6)...)
-        u0 = rand(fa.mesh.nξ, 2)
+        u0 = ArrayPartition([rand(fa.mesh.nξ) for _ in 1: 2]...)
         prob, = assemble(fa, p, u0, (0., 1.0); flf=CForm())
         du = similar(u0)
         @inferred prob.f(du, u0, prob.p, 1.0)
@@ -13,7 +13,7 @@ using Test
     @testset "2D fault" begin
         fa = fault(Val(:RectOkada), STRIKING(), 10., 10., 2., 2.)
         p = ElasticRSFProperty([rand(fa.mesh.nx, fa.mesh.nξ) for _ in 1: 4]..., rand(6)...)
-        u0 = rand(fa.mesh.nx, fa.mesh.nξ, 2)
+        u0 = ArrayPartition([rand(fa.mesh.nx, fa.mesh.nξ) for _ in 1: 2]...)
         prob, = assemble(fa, p, u0, (0., 1.0); buffer_ratio=1)
         du = similar(u0)
         @inferred prob.f(du, u0, prob.p, 1.0)
