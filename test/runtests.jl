8600caaa462141efcfce543dba2b6f85f507028f
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-07-25 13:24:14 -0400

    step 1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
 using JuEQ
+using GmshTools
+using Test
 
 # const TESTDIR = @__DIR__
 # const TESTFILES = filter(x -> startswith(x, "test_") && endswith(x, ".jl"), readdir(TESTDIR))
 # foreach(x -> include(joinpath(TESTDIR, x)), TESTFILES)
 
-using GmshTools
 try
     gmsh.initialize()
     @test true
