1288dbeffa910bb2f2a04eaac1c8e4d14a4cdfa2
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-07-25 12:57:32 -0400

    test weird fail on linux

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
 using JuEQ
 
-const TESTDIR = @__DIR__
-const TESTFILES = filter(x -> startswith(x, "test_") && endswith(x, ".jl"), readdir(TESTDIR))
-foreach(x -> include(joinpath(TESTDIR, x)), TESTFILES)
+# const TESTDIR = @__DIR__
+# const TESTFILES = filter(x -> startswith(x, "test_") && endswith(x, ".jl"), readdir(TESTDIR))
+# foreach(x -> include(joinpath(TESTDIR, x)), TESTFILES)
+
+using GmshTools
+try
+    gmsh.initialize()
+    @test true
+catch
+    @test false
+finally
+    gmsh.finalize()
+end
